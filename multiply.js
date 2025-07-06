685c88620f7ecefb93b1dcc4f40edf6a7754c886
sarahangwen <angwensarahsunday@gmail.com>
2025-07-06 19:52:32 +0300

    made a successful test

--- multiply.js
+++ multiply.js
@@ -11,7 +11,7 @@
 // module.exports={multiply};
 
 
-// function multiply(a,b){
-//     return a * b;
-// }
-// module.exports={multiply};
+function multiply(a,b){
+    return a * b;
+}
+module.exports={multiply};
