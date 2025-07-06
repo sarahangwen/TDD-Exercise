3b34f01db4ff637291b87a5a7411506d88bb1c79
sarahangwen <angwensarahsunday@gmail.com>
2025-07-06 19:35:11 +0300

    After the failure, the test was successful

--- multiply.js
+++ multiply.js
@@ -3,7 +3,7 @@
 // }
 // module.exports = { multiply };
 
-// function multiply(a,b){
-//     return a * b;
-// }
-// module.exports={multiply};
+function multiply(a,b){
+    return a * b;
+}
+module.exports={multiply};
