0c61459127cc9d6c2987b9bc9092a99e96779c62
sarahangwen <angwensarahsunday@gmail.com>
2025-07-07 20:22:42 +0300

    Remove node_modules and update .gitignore

--- test/test.js
+++ test/test.js
@@ -1,53 +1,41 @@
 //Pair
 //Sarah Angwen and Kabwaga Vereonica
+const assertEqual = require("assert");
+const { multiply } = require("../multiply");
+describe("multiply", () => {
+  it("Should return 1 when multiplying 1 and 1", () => {
+    assertEqual(multiply(1, 1), 1);
+  });
+});
 
-
-
-
-
-// const assertEqual = require('assert');  
-// const { multiply } = require('../multiply'); 
-// describe('multiply', () => {
-//   it('Should return 1 when multiplying 1 and 1', () => {
-//     assertEqual(multiply(1, 1), 1);  
-//   });
-// });
-
-
-
-// const assertEqual = require('assert');  
-// const { multiply } = require('../multiply'); 
+// const assertEqual = require('assert');
+// const { multiply } = require('../multiply');
 // describe('multiply',()=>{
 //     it('Should return 4 when when we multiply 2 times 2',()=>{
 //         assertEqual(multiply(2, 2),4)
 //     })
 // })
 
-
-
-// const assertEqual = require('assert');  
+// const assertEqual = require('assert');
 // const { multiply } = require('../multiply');
 // describe('multiply',()=>{
 //     it('should return 9 when we muliply 3 times 3',()=>{
-//        assertEqual(multiply(3, 3), 9) 
+//        assertEqual(multiply(3, 3), 9)
 //     })
 // })
 
-
-// const assertEqual = require('assert');  
+// const assertEqual = require('assert');
 // const { multiply } = require('../multiply');
 // describe('multiply',()=>{
 //     it('should return 16 when we muliply 4 times 4',()=>{
-//        assertEqual(multiply(4, 4), 16) 
+//        assertEqual(multiply(4, 4), 16)
 //     })
 // })
 
-const assertEqual = require('assert');
-const { multiply } = require('../multiply');
-
-describe('multiply', () => {
-    it('Should return 1035 when we multiply 23 times 45', () => {
-        assertEqual(multiply(23, 45), 1035);
-    });
-});
-
+// const assertEqual = require('assert');
+// const { multiply } = require('../multiply');
+// describe('multiply', () => {
+//     it('Should return 1035 when we multiply 23 times 45', () => {
+//         assertEqual(multiply(23, 45), 1035);
+//     });
+// });
