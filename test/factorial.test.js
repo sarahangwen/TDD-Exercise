2eff6c2386ca0e3fbe8474bb106b0af744fea49a
sarahangwen <angwensarahsunday@gmail.com>
2025-07-07 22:02:04 +0300

    madd a succesful test

--- test/factorial.test.js
+++ test/factorial.test.js
@@ -4,15 +4,17 @@
 const assert = require('assert');
 const { factorial } = require('../factorial');
 
+// describe('factorial', () => {
+//   it('should return 1 for 1!', () => {
+//     assert.strictEqual(factorial(1), 1);
+//   });
+// })
+
 describe('factorial', () => {
-  it('should return 1 for 1!', () => {
-    assert.strictEqual(factorial(1), 1);
+  it('should return 6 for 3!', () => {
+    assert.strictEqual(factorial(3), 6);
   });
 })
-
-  // it('should return 6 for 3!', () => {
-  //   assert.strictEqual(factorial(3), 6);
-  // });
 
   // it('should return 24 for 4!', () => {
   //   assert.strictEqual(factorial(4), 24);
