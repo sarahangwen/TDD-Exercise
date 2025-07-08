5754338599544f653b9ac3f3f2d1edd365756573
sarahangwen <angwensarahsunday@gmail.com>
2025-07-08 21:36:01 +0300

    The test has failed again because the expected value is diffrerent from the actual value

--- fibonacci.js
+++ fibonacci.js
@@ -1,8 +1,8 @@
 // fibonacci.js
 function fibonacci(n) {
-  // if (n < 0) throw new Error("Fibonacci not defined for negative numbers");
-  // if (n === 0) return 0;
-  // if (n === 1) return 1;
-  // return fibonacci(n - 1) + fibonacci(n - 2);
+  if (n < 0) throw new Error("Fibonacci not defined for negative numbers");
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 module.exports = { fibonacci };
