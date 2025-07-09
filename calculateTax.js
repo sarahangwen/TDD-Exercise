3f22b8a14ae7db0220b6b394d0bc6729dd8e76f8
sarahangwen <angwensarahsunday@gmail.com>
2025-07-09 10:53:52 +0300

    The test was successful after passing the function to be tested

--- calculateTax.js
+++ calculateTax.js
@@ -9,7 +9,15 @@
 // }
 
 
+function calculateTax(income) {
+  if (income < 12000) return 0;
+  if (income <= 36000) return (income - 12000) * 0.2;
+  return (36000 - 12000) * 0.2 + (income - 36000) * 0.4;
+}
 
-// module.exports = { calculateTax };
+module.exports = { calculateTax };
+
+
+
 
 
--- test/caculateTax.test.js
+++ test/caculateTax.test.js
@@ -10,7 +10,7 @@ describe('calculateTax', () => {
   assert.equal(calculateTax(20000), (20000 - 12000) * 0.2); 
 });
 
-  it('should return correct tax for income greater than 36000', () => {
+  it('calculateTax(income)=tax', () => {
   const income = 50000;
   const tax = (36000 - 12000) * 0.2 + (50000 - 36000) * 0.4; 
   assert.strictEqual(calculateTax(income), tax);
