961171750568685ad92dfbc17f313e348f63a7a1
sarahangwen <angwensarahsunday@gmail.com>
2025-07-09 10:47:48 +0300

    The test is successfull becasue finally the function was called for testing.

--- calculateTax.js
+++ calculateTax.js
@@ -3,11 +3,11 @@
 // }
 
 
-// function calculateTax(income) {
-//   if (income < 12000) return 0;
-//   if (income <= 36000) return (income - 12000) * 0.2;
-// }
+function calculateTax(income) {
+  if (income < 12000) return 0;
+  if (income <= 36000) return (income - 12000) * 0.2;
+}
 
-// module.exports = { calculateTax };
+module.exports = { calculateTax };
 
 
